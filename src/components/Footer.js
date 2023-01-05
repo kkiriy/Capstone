94ca589f3eba7ea6f8a64484da3d59c4fd2bb8ac
kkiriy <k.kiriy@gmail.com>
2023-01-05 15:23:39 +0200

    footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,4 +1,31 @@
-function Footer() {
+import logo from "../assets/logo_ft.png"
+import Nav from "./Nav";
 
+function Footer() {
+    return (
+    <footer>
+        
+        <div id="logo_ft">
+        <img src={logo} alt="Little Lemon" />
+        </div>
+        <div>
+            <h3>Doormat Navigation</h3>
+            <Nav />
+        </div>
+        <div>
+            <h3>Contact</h3>
+            Adress <br></br>
+            phone number<br></br>
+            email
+        </div>
+        <div>
+            <h3>Social Media Links</h3>
+            Adress <br></br>
+            phone number<br></br>
+            email
+        </div>
+        
+    </footer>
+    )
 }
 export default Footer;
